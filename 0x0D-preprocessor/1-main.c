4452e7c378f4e5c91bb5448b8a70de60e322ff8d
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-06-19 09:50:27 -0700

    C - Preprocessor tasks

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,18 @@
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return 0;
+}
