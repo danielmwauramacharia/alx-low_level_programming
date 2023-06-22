638a8174d65f8de3ea0633ff17619cb767e1df0d
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-06-22 05:25:22 -0700

    variadic functions in c programming

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,58 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - Prints anything based on the format provided.
+ * @format: A list of types of arguments passed to the function.
+ *
+ * Description: The supported format specifiers are:
+ *              - 'c' for char
+ *              - 'i' for int
+ *              - 'f' for float
+ *              - 's' for string
+ *              Any other character is ignored.
+ *              If a string argument is NULL, "(nil)" is printed.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
+}
