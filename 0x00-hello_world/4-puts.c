842c30e86190c28266c7fc56bc6f82ed9d787ffd
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-17 17:09:32 -0800

    the first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point of the function
+ *
+ * Return :alyways 0 on sucess
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
+
