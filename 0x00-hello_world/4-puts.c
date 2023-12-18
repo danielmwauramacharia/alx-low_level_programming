44708e780091d895c2cc471a8f72b68d8a8c4608
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-17 17:37:03 -0800

    bug fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main- A function that uses puts
+ * main - Entry point of the program
  *
- * Return :alyways 0(success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -11,5 +11,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
-
