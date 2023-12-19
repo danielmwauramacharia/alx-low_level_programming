1c042260fc282983008a3a982ae29df6d82e79ea
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-19 07:54:48 -0800

    fix bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,8 @@
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, "
-                          "2015-10-19";
+	write(2, 
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-    write(2, message, sizeof("and that piece of art is useful\" - Dora Korpar, "
-                             "2015-10-19"));
-
-    return (1);
+	return (1);
 }
