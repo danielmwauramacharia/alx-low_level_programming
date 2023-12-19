6a8d1c6acabca221f71138d02a1aa4a535a343b0
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-19 06:11:31 -0800

    fix bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <unistd.h>
 
 /**
-  *main - Entry point of our program
-  *
-  *Return: 1(success)
-  */
-
+ * main - Entry point of our program
+ * Return: 1 (success)
+ */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    const char *message = "and that piece of art is useful\" - Dora Korpar, "
+                          "2015-10-19";
 
-	write(1, message, sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+    write(2, message, sizeof("and that piece of art is useful\" - Dora Korpar, "
+                             "2015-10-19"));
 
-	return (1);
-
+    return (1);
 }
