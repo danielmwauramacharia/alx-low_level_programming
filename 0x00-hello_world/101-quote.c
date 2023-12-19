fbadf9848d4f65b1c463d2674cc8d39d79b7bdcd
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-19 08:27:33 -0800

    trying the pointer way

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,10 @@
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	const char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
 
 	return (1);
 }
