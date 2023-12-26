4fc9d383f926545b7a2004bd8ceb520473a97572
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-25 17:16:28 -0800

    three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+{
+	for (j = i + 1; j <= 8; j++)
+{
+		for (k = i + 2; k <= 9; k++)
+{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar('0' + k);
+			if (i != 7 || j != 8 || k != 9)
+{
+				putchar(',');
+				putchar(' ');
+}
+}
+}
+}
+	putchar('\n');
+	return (0);
+
+}
+
