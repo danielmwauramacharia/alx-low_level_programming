6efe3efb050d17e120d6fcf22b4ccf4886a32b9f
danielmwauramacharia <mwauradaniel06@gmail.com>
2023-12-27 07:56:20 -0800

    print lower case alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 -prints alphabets in lowercase 10 times
+  *
+  */
+
+void print_alphabet_x10(void)
+{
+	char alphabets;
+	char a;
+	
+	for (a = 0; a < 10; a++)
+{
+		for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+{
+			_putchar(alphabets);
+}
+	_putchar('\n');
+}
+}
+
