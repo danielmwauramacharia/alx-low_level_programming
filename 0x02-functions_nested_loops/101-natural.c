c72043c34730917ac34cd2d55eed0e76f90a8ecd
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-02 18:05:59 -0800

    adds numbers if they are multiple of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main -entry to our program
+  *
+  *Return: 0 (success)
+  */
+
+int main(void)
+{
+	int i, mult3, mult5, sum;
+
+	for (i = 1; i < 1024; i++)
+{
+		mult3 = i % 3;
+		mult5 = i % 5;
+
+		if (mult3 == 0 || mult5 == 0)
+{
+			sum += i;
+}
+}
+	printf("%d\n", sum);
+	return (0);
+}
