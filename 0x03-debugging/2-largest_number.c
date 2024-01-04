a89246b6ae1ead36add345566f98ed6d0cacd8f3
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-04 03:03:11 -0800

    picks the largest number among three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,43 +10,15 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = a;
 
-	if (a > b && a > c)
-{
-		largest = a;
-}
-	if (b > a && b > c)
+	if (b > largest)
 {
 		largest = b;
 }
-	if (c > a && c > b)
+	if (c > largest)
 {
 		largest = c;
-}
-	if (a == b && b > c)
-{
-		largest = b;
-}
-	else
-{
-		largest = c;
-}
-	if (a == c && a > b)
-{
-		largest = a;
-}
-	else
-{
-		largest = b;
-}
-	if (c == b && b > a)
-{
-		largest = b;
-}
-	else
-{
-		largest = a;
 }
 	return (largest);
 }
