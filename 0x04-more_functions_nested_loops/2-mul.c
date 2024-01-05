d41515bcdc89638bc1805dd5899dc4c8d790caef
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-05 03:24:17 -0800

    added the coreect file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *mul -multiplies two numbers
+  *@a: the first number
+  *@b: the second number
+  *
+  *Return: Result
+  */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
