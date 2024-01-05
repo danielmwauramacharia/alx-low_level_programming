1ba8c675d7007823f1506d5b6a1e057ecf7efb56
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-04 21:27:34 -0800

    bug fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,12 +8,12 @@
   */
 int _isdigit(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c < 0 && c > 9))
+	if (c >= '0' && c <= '9')
 {
-	return (0);
+		return (1);
 }
 	else
 {
-		return (1);
+		return (0);
 }
 }
