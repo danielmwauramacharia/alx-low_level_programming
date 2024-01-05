0fe36edf25ce7e643a8bd3456a96c28c2d1d6030
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-04 19:42:47 -0800

    checks for upper character and return 1 and 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *_isupper -Checks for upper character
+  *@c: The character to be checked
+  *
+  *Return: 1 success 0 otherwise
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+{
+		return (1);
+}
+	else
+{
+		return(0);
+}
+}
