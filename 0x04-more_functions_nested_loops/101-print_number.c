84003946016d7c5799e039a9cd8f4eb4dcc21894
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-09 04:46:47 -0800

    bug fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 	if (n == INT_MIN)
 {
 		_putchar('-');
-		print_number(-(n/10));
+		print_number(-(n / 10));
 		_putchar('0' + (-(n % 10)));
 }
 	else
@@ -25,5 +25,5 @@ void print_number(int n)
 			print_number(n / 10);
 }
 }
-	_putchar(n % 10 + '0');
+		_putchar(n % 10 + '0');
 }
