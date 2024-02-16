d3d3e4d04187d5c4873feb2d2afcb7bbb5f52f1a
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-02-16 08:27:04 -0800

    Bug fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,39 @@
 #include "main.h"
 #include <limits.h>
-
 /**
-  *print_number-function that prints intergers
-  *@n: the integer to be printed
+  *print_number -prints any passed integer
+  *@n: the passed integer
+  *
   */
 void print_number(int n)
 {
+	const int a = n;
+	char buffer[20];
+	int i = 0, j, digit, l;
+
+	if (n == 0)
+		_putchar('0');
+	if ((n < 0) && (!(n == INT_MIN)))
+{
+		_putchar('-');
+		n = -n;
+}
 	if (n == INT_MIN)
 {
 		_putchar('-');
-		print_number(-(n / 10));
-		_putchar('0' + (-(n % 10)));
+		l = -(n % 10);
+		n = -(n / 10);
 }
-	else
+	while (n != 0)
 {
-		if (n < 0)
+		digit = n % 10;
+		buffer[i++] = digit + '0';
+		n /= 10;
+}
+	for (j = i - 1; j >= 0; j--)
 {
-			_putchar('-');
-			n = -n;
+		_putchar(buffer[j]);
 }
-		if (n / 10)
-{
-			print_number(n / 10);
-}
-}
-		_putchar(n % 10 + '0');
+	if (a == INT_MIN)
+		_putchar(l + '0');
 }
