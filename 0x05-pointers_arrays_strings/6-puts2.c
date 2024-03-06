5d0f5b243969588937427a1512f41bef6d06ba0a
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-03-06 02:23:54 -0800

    bug fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: the input string
- */
+  *puts2 - function that prints every other character in a string
+  *@str: The string to be printed
+  *
+  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, j = 0, count = -1, len;
 
 	while (str[i] != '\0')
 {
-		if (str[i] >= 32 && str[i] <= 126)
-{
-			_putchar(str[i]);
+		count++;
+		i++;
 }
-		i += 2;
+	len = count;
+	while (j <= len)
+{
+		_putchar(str[j]);
+		j = j + 2;
 }
 	_putchar('\n');
 }
