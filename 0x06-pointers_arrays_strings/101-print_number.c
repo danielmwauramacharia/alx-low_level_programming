e109d8e3f6730b7a8a782b542ccb6b31fdbbe394
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-03-08 04:08:42 -0800

    bug fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,38 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * print_number - prints an integer
- * @n: the integer to be printed
- */
+  *print_number - Function that prints an integer
+  *@n: The integer to be printed
+  *
+  *Description: The function doest use long data type
+  *The function also does not use array or pointer
+  */
 void print_number(int n)
 {
+	int divisor = 1, digit = 0, is_min = 0, l = 0;
+
+	if (n == INT_MIN)
+{
+		_putchar('-');
+		is_min = 1;
+		l = -(n % 10);
+		n = -(n / 10);
+}
 	if (n < 0)
 {
 		_putchar('-');
 		n = -n;
 }
-
-	if (n / 10 != 0)
+	while (n / divisor >= 10)
+		divisor = divisor * 10;
+	while (divisor != 0)
 {
-		print_number(n / 10);
+		digit = n / divisor;
+		_putchar(digit + '0');
+		n = n % divisor;
+		divisor = divisor / 10;
 }
-	_putchar((n % 10) + '0');
+	if (is_min)
+		_putchar(l + '0');
 }
