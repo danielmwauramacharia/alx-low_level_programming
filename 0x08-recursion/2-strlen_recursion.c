cb7f33db4219a69d7a7b05a3ec0427150947cf3e
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-03-24 19:08:49 -0700

    recursive function to calculate length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *_strlen_recursion - Function that calulates the length of a string
+  *@s: The string
+  *
+  *Return: The count
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
