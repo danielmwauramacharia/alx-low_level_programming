7bde82a065d2f105724652b34318b8470f1d1b9d
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-03-26 19:53:02 -0700

    Bug fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,33 @@
 #include "main.h"
 /**
   *counter - A helper function that helps return the length of a string
-  *@s: The pointer to the string
+  *@org: The pointer to the string
   *
   *Return: The length
   */
 int counter(char *org)
 {
 	if (*org == '\0')
-		return (1);
+		return (0);
 	return (1 + counter(org + 1));
 }
+/**
+  *helper - A helper function that checks if s is a palindrom
+  *@s: The string
+  *@start: the starting index
+  *@end: The last index
+  *
+  *Return: 1 if palindrome 0 0therwise
+  */
+int helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (helper(s, start + 1, end - 1));
+}
+
 /**
   *is_palindrome - Checks if a string is a palindrome or not
   *@s: Pointer to the string
@@ -19,22 +36,7 @@ int counter(char *org)
   */
 int is_palindrome(char *s)
 {
-	int count, i, j;
-	char *org = s;
+	int length = counter(s);
 
-	count = counter(org);
-	i = count;
-	j = count;
-	if (i == 1)
-		return (1);
-	if (s[j - 1] != *s && (*s != '\0'))
-		return (0);
-	else if (s[j - 1] == *s && (*s != '\0'))
-{
-		j--;
-		s++;
-		return (is_palindrome(s));
-}
-	else
-		return (1);
+	return (helper(s, 0, length - 1));
 }
