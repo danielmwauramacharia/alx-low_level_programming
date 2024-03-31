bb9301f5f6c3d31811edad7b84844670ab12d292
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-03-31 13:58:50 -0700

    a cl arguments that prints the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- * @argc: Number of arguments passed to the program
- * @argv: Array of strings containing the command-line arguments
- *
- * Description: Prints the name of the program, followed by a new line.
- * If the program is renamed, it will still print the new name.
- *
- * Return: Always 0 (indicating successful execution)
- */
+  *main -Entry point of our programme
+  *@argc: Counts the number of arguments passed in the command line
+  *@argv: Stores the the commands passed as strings
+  *
+  *Description: Prints the name of a program followed by a new line
+  *The printed name changes if the programme is renamed without re compiling
+  *
+  *Return: 0 if success
+  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
 	return (0);
 }
