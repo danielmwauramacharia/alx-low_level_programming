3dd779e312fb8db791e2f7a40dd49789f0ed31cb
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-02 04:59:40 -0700

    bug fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,10 +20,9 @@ char *_strdup(char *str)
 	ptr = ((char *)malloc(n * sizeof(char)));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < count; i++)
+	for (i = 0; i < n; i++)
 {
 		*(ptr + i) = str[i];
 }
-	ptr[count] = '\0';
 	return (ptr);
 }
