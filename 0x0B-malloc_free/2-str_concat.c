910f1315f86c9f6ef0164819cf76fa709928ba90
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-04 05:08:28 -0700

    bug fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@
 char *str_concat(char *s1, char *s2)
 {
 	int n = 0, d = 0;
-	/*int a = 0, b = 0, c = 0;*/
+	int a = 0, b = 0, c = 0, i = 0;
 	int count1 = 0, count2 = 0, j = 0, k = 0, l = 0;
 	char *ptr;
 
-/*	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 {
 		a = 1;
 		n = 1;
@@ -25,12 +25,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = "";*/
+		s2 = "";
 	while (s1[count1] != '\0')
 		count1++;
 	while (s2[count2] != '\0')
 		count2++;
-	/*if (count1 == 0 && count2 != 0)
+	if (count1 == 0 && count2 != 0)
 {
 		n = count2 + 1;
 		b = 1;
@@ -39,7 +39,7 @@ char *str_concat(char *s1, char *s2)
 {
 		c = 1;
 		n = count1 + 1;
-}*/
+}
 	if (count1 != 0 && count2 != 0)
 {
 		n = count1 + count2 + 2;
@@ -48,7 +48,7 @@ char *str_concat(char *s1, char *s2)
 	ptr = ((char *)malloc(n * sizeof(char)));
 	if (ptr == NULL)
 		return (NULL);
-/*	if (a)
+	if (a)
 {
 		ptr = "";
 }
@@ -63,28 +63,19 @@ char *str_concat(char *s1, char *s2)
 		for (i = 0; i < n; i++)
 			*(ptr + i) = s1[i];
 		*(ptr + i) = '\0';
-}*/
+}
 	if (d)
 {
 	while (j < count1)
 {
-		if (l == count1 - 1 && s1[j] == ' ')
+		*(ptr + l) = s1[j];
+		l++;
+		if (s1[j] == ' ' && j != count1 - 1)
 {
-			*(ptr + l) = s1[j];
-			l++;
-			j++;
-			continue;
-}
-		else if (l == count1 - 1 && s1[j] != ' ')
-{
-			*(ptr + l) = s1[j];
-			l++;
 			*(ptr + l) = ' ';
 			l++;
-			j++;
-			continue;
 }
-		*(ptr + l) = s1[j];
+		j++;
 }
 	while (k < count2)
 {
