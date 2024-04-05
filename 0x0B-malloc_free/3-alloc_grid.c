b913a49cc7df4fdaa66fd95e43b0c5f2357615c7
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-04 17:39:42 -0700

    allocating the right memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	ptr = ((int **)malloc(height * sizeof(int *)));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 {
 		ptr[i] = ((int *)malloc(width * sizeof(int)));
 		if (ptr[i] == NULL)
